395e4830a0c6b87e53bdfffebf53672637718b4f
GhoraiVanga <48147708+GhoraiVanga@users.noreply.github.com>
2020-07-27 16:21:28 +0530

    make it better

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/shop'));
+app.use(express.static('./dist/personal-portfoli-angular'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/shop/'}),
+    res.sendFile('index.html', {root: 'dist/personal-portfoli-angular/'}),
 );
 
 app.listen(process.env.PORT || 8080);
